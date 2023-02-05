d216072823183eee10209afd7a7a16aaeb185741
Daniel <danielius.s2601@gmail.com>
2023-02-05 18:01:14 +0100

    build: backend server using flask

---
+++ backend/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Members API Route
+
+@app.route('/members')
+def members():
+    return {'members': ['John', 'Paul', 'George', 'Ringo']}
+
+if __name__ == '__main__':
+    app.run(debug=True)
